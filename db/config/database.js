1f476d29de5a6ffe11ca232588990831d3201a98
Oleg Grachev <91218806+olegTheCoder@users.noreply.github.com>
2021-11-19 16:37:40 +0200

    Update database.js

--- db/config/database.js
+++ db/config/database.js
@@ -21,5 +21,8 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: {rejectUnauthorized: false},
+    },
   },
 };
